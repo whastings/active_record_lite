0785a5a67f9ddb3dc43c7d4aa6f7309775061d68
Will Hastings <will@objectdotcreate.net>
2014-02-14 12:41:50 -0800

    Implement Searchable#where and extend SQLObject with Searchable.

--- lib/active_record_lite/03_searchable.rb
+++ lib/active_record_lite/03_searchable.rb
@@ -3,10 +3,21 @@ require_relative '02_sql_object'
 
 module Searchable
   def where(params)
-    # ...
+    where_string = params.map { |param, _| "#{param} = ?" }
+    where_string = where_string.join(' AND ')
+    where_query = <<-SQL
+      SELECT
+        #{self.table_name}.*
+      FROM
+        #{self.table_name}
+      WHERE
+        #{where_string}
+    SQL
+    results = DBConnection.execute(where_query, *params.values)
+    self.parse_all(results)
   end
 end
 
 class SQLObject
-  # Mixin Searchable here...
+  extend Searchable
 end

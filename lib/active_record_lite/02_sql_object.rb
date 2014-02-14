444e4b0589fbd40e6ae78033a6840b2e583286f8
Will Hastings <will@objectdotcreate.net>
2014-02-14 10:34:45 -0800

    Define initialization logic for SQLObject, including creation of accessors from table columns.

--- lib/active_record_lite/02_sql_object.rb
+++ lib/active_record_lite/02_sql_object.rb
@@ -1,24 +1,40 @@
 require_relative 'db_connection'
 require_relative '01_mass_object'
+require_relative '00_attr_accessor_object'
 require 'active_support/inflector'
 
-class MassObject
+class MassObject < AttrAccessorObject
   def self.parse_all(results)
-    # ...
+    results.map do |result|
+      self.new(result)
+    end
   end
 end
 
 class SQLObject < MassObject
   def self.columns
-    # ...
+    return @columns if @columns
+    columns_query = <<-SQL
+      SELECT
+        *
+      FROM
+        #{self.table_name}
+      LIMIT
+        1
+    SQL
+    attributes = DBConnection.execute2(columns_query).first.map(&:to_sym)
+    my_attr_accessor(*attributes)
+    @columns = attributes
+    attributes
   end
 
   def self.table_name=(table_name)
-    # ...
+    @table_name = table_name
   end
 
   def self.table_name
-    # ...
+    return @table_name if @table_name
+    @table_name = self.to_s.underscore.pluralize
   end
 
   def self.all
@@ -30,15 +46,23 @@ class SQLObject < MassObject
   end
 
   def attributes
-    # ...
+    return @attributes if @attributes
+    @attributes = {}
   end
 
   def insert
     # ...
   end
 
-  def initialize
-    # ...
+  def initialize(params)
+    columns = self.class.columns
+    params.each do |name, value|
+      name = name.to_sym
+      unless columns.include?(name)
+        raise ArgumentError, "Unknown attribute #{name} for #{self.class}"
+      end
+      self.send("#{name}=", value)
+    end
   end
 
   def save
@@ -52,4 +76,8 @@ class SQLObject < MassObject
   def attribute_values
     # ...
   end
+
+  private
+
+
 end
